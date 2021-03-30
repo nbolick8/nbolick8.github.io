f89be8aa1d9e181f9a6fb5afc7c07b9e0c837bb8
nbolick8 <nbolick8@uncc.edu>
2021-03-29 22:48:46 -0400

    Update map.js

--- map.js
+++ map.js
@@ -1,6 +1,12 @@
+// This example adds a search box to a map, using the Google Place Autocomplete
+// feature. People can enter geographical searches. The search box will return a
+// pick list containing a mix of places and predicted search terms.
+// This example requires the Places library. Include the libraries=places
+// parameter when you first load the API. For example:
+// <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
 function initAutocomplete() {
     const map = new google.maps.Map(document.getElementById("map"), {
-      center: { lat: 35.2033529, lng: -80.9802558 },
+      center: { lat: -33.8688, lng: 151.2195 },
       zoom: 13,
       mapTypeId: "roadmap",
     });
