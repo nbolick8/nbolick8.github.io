b011e6a683e9a700b634e84c3944b8a60675624b
nbolick8 <nbolick8@uncc.edu>
2021-03-29 20:56:15 -0400

    Trying to get default map to work

--- map.js
+++ map.js
@@ -4,35 +4,9 @@
 let map;
 let service;
 let infowindow;
-function showLocation(position) {
-    var latitude = position.coords.latitude;
-    var longitude = position.coords.longitude;
-    var latlongvalue = position.coords.latitude + ","
-    + position.coords.longitude;
-    var img_url = "https://maps.googleapis.com/maps/api/staticmap?center="
-    +latlongvalue+"&zoom=14&size=400x300&key=AIzaSyCQSdFAeq7AqRbl8pCR2gGc-dGDhxxLNpo";
-    document.getElementById("mapholder").innerHTML =
-    "<img src='"+img_url+"'>";
- }
- function errorHandler(err) {
-    if(err.code == 1) {
-       alert("Error: Access is denied!");
-    } else if( err.code == 2) {
-       alert("Error: Position is unavailable!");
-    }
- }
- function getLocation(){
-    if(navigator.geolocation){
-       // timeout at 60000 milliseconds (60 seconds)
-       var options = {timeout:60000};
-       navigator.geolocation.getCurrentPosition
-       (showLocation, errorHandler, options);
-    } else{
-       alert("Sorry, browser does not support geolocation!");
-    }
- }
+
 function initMap() {
-  const sydney = new google.maps.LatLng(latitude, longitude);
+  const sydney = new google.maps.LatLng(-33.867, 151.195);
   infowindow = new google.maps.InfoWindow();
   map = new google.maps.Map(document.getElementById("map"), {
     center: sydney,
