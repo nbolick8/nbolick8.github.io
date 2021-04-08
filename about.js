cfc87b4aaf170f35ad34d88e621e2e6061583c48
jgeneta <jgeneta@uncc.edu>
2021-04-08 16:58:22 -0400

    Implementing Firebase

---
+++ about.js
@@ -0,0 +1,51 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCOLuXau4mjrAoHPuouikZljO57C7oAOQE",
+    authDomain: "registration-f02c9.firebaseapp.com",
+    projectId: "registration-f02c9",
+    storageBucket: "registration-f02c9.appspot.com",
+    messagingSenderId: "871692680552",
+    appId: "1:871692680552:web:113e720f23c92ff3402add"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+    const auth = firebase.auth();
+
+    function signUp(){
+
+        var email = document.getElementById("email");
+        var password = document.getElementById("password");
+
+        const promise = auth.createUserWithEmailAndPassword(email.value, password.value);
+        promise.catch(e => alert(e.message));
+
+        alert("Successfully Registered!");
+    }
+
+    function signIn(){
+
+        var email = document.getElementById("email");
+        var password = document.getElementById("password");
+
+        const promise = auth.signInWithEmailAndPassword(email.value, password.value);
+        promise.catch(e => alert(e.message));
+
+        alert("Successfully Logged In!");
+    }
+
+    function signOut() {
+        auth.signOut();
+        alert("Successfully Signed Out!");
+    }
+
+    auth.onAuthStateChanged(function(user) {
+        if(user) {
+            var email = user.email;
+            alert("Active User" + email);
+        } else {
+            alert("No Active User");
+        }
+
+    });
+
