bf2a7773c4c52ecad4d469a83177e8cbadaa4d59
jgeneta <jgeneta@uncc.edu>
2021-04-14 12:41:15 -0400

    update

--- about.js
+++ about.js
@@ -30,6 +30,7 @@
 
         const promise = auth.signInWithEmailAndPassword(email.value, password.value);
         promise.catch(e => alert(e.message));
+        
 
         //alert("Logging In");
 
