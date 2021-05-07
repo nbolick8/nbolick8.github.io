ab3a1e5f2e53dfe9b8d4243c8d1782d9c0457a52
Justin Ganey <jganey1@uncc.edu>
2021-05-06 23:02:45 -0400

    Update addEvent.js

--- addEvent.js
+++ addEvent.js
@@ -1,11 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var calendarEl = document.getElementById('calendar');
-    var calendar = new FullCalendar.Calendar(calendarEl, {
-      initialView: 'dayGridMonth'
-    });
-    calendar.render();
-  });
-  
+
 $(document).ready(function() {
 
     $('#calendar').fullCalendar({
