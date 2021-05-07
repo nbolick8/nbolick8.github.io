5d66164149a941f05e6c78f5eca9d99363c7ae73
Justin Ganey <jganey1@uncc.edu>
2021-05-06 23:49:53 -0400

    Update main.js

--- main.js
+++ main.js
@@ -21,7 +21,35 @@ var FullCalendar = (function (exports) {
     PERFORMANCE OF THIS SOFTWARE.
     ***************************************************************************** */
     /* global Reflect, Promise */
-
+    $(document).ready(function() {
+        ShowCalendar();
+      });
+      
+      var events = [];
+      var calendarEl = document.getElementById('calendar');
+      var calendar = new FullCalendar.Calendar(calendarEl, {
+      
+          initialView: 'dayGridMonth',
+      
+          events: function(info, successCallback, failureCallback ) {
+            successCallback(events);
+          },
+      
+        });
+      
+      function ShowCalendar() {
+        calendar.render();
+      }
+      
+      $("#addEvent").on("click", function() {
+        events.push({
+          title: $("#eventName").val(),
+          start: $("#fromDate").val(),
+          end: $("#toDate").val()
+        });
+      
+        calendar.refetchEvents();
+      });
     var extendStatics = function(d, b) {
         extendStatics = Object.setPrototypeOf ||
             ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
