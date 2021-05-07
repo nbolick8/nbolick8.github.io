c026f67aac33309d8a4ebf0b7748e21de8316859
Justin Ganey <jganey1@uncc.edu>
2021-05-06 21:46:01 -0400

    calendar

--- forum.js
+++ forum.js
@@ -15,109 +15,398 @@ const settings = {
 };
 db.settings(settings);
 
-const form = document.querySelector("form");
-const nickname = document.getElementById("nickname");
-const message = document.getElementById("message");
-const errorMessage = document.querySelector(".error-message");
-const closebtn = document.querySelector(".error-message .close");
-const dataArea = document.getElementById("load-data");
+var events = []
+events=parselocalstorage('events')
+var renderPopup = function (jsEvent, start, end, calEvent) {
+  var $popup = $('#calendar-popup');
+  var $eventForm = $('#event-form');
+  $event = $('#event');
+  var $selectedElmt = $(jsEvent.target);
 
-form.addEventListener("submit", e => {
-  e.preventDefault();
+  var relativeStartDay = start.calendar(null, { lastDay: '[yesterday]', sameDay: '[today]'});
+  var endNextDay = '';
 
-  if (nickname.value !== "" && message.value !== "") {
-    db
-      .collection("messages")
-      .add({
-        nickname: nickname.value,
-        message: message.value,
-        date: new Date()
-      })
-      .then(docRef => {
-        console.log("Document written with ID: ", docRef.id);
-        // window.location.reload();
-      })
-      .catch(error => {
-        console.error("Error adding document: ", error);
-      });
-    errorMessage.classList.remove("show");
-    nickname.value = "";
-    message.value = "";
-  } else {
-    errorMessage.classList.add("show");
+  if(relativeStartDay === 'yesterday') {
+    endNextDay = '[Today at] ';
   }
-});
+  else if(relativeStartDay === 'today') {
+    endNextDay = '[Tomorrow at] ';
+  }
+  else {
+    endNextDay = 'dddd ';
+  }
 
-closebtn.addEventListener("click", () => {
-  errorMessage.classList.remove("show");
-});
-
-// A function for formatting a date to DD Month YY - HH:mm
-formatDate = d => {
-  // Months array to get the month in string format
-  const months = new Array(
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December"
+  $('.start-time').html(
+    ' <p><i class="fa fa-play" aria-hidden="true"></i>' + (start.isSameOrBefore(moment()) ? 'Started' : 'Starts') + '</p>'
+      + '<time datetime="' + start.format() + '">'
+      + start.calendar(null, {
+        lastWeek: 'L LT',
+        nextWeek: 'dddd LT',
+        sameElse: 'L LT'
+      })
+      + '</time>'
   );
-  // get the month
-  const month = d.getMonth();
-  // get the day
-  const day = d.getDate();
-  // get the year
-  let year = d.getFullYear();
-  // pull the last two digits of the year
-  year = year.toString().substr(-2);
-  // get the hours
-  const hours = d.getHours();
-  // get the minutes
-  const minutes = ("0" + d.getMinutes()).slice(-2);
-  //return the string "DD Month YY - HH:mm"
-  return (
-    day + " " + months[month] + " '" + year + " - " + hours + ":" + minutes
+  $('.end-time').html(
+    '<p><i class="fa fa-stop" aria-hidden="true"></i> '
+      + (end.isSameOrBefore(moment()) ? 'Ended' : 'Ends')
+      + (end.isSame(start, 'day') ? ' at' : '')
+      + '</p>'
+      + '<time datetime="' + end.format() + '">'
+      + end.calendar(start, {
+        sameDay: 'LT',
+        nextDay: endNextDay + 'LT',
+        nextWeek: 'dddd LT',
+        sameElse: 'L LT'
+      })
+      + '</time>'
   );
-};
 
-db
-  .collection("messages")
-  .orderBy("date")
-  .onSnapshot(querySnapshot => {
-    let messages = [];
-    querySnapshot.forEach(chat => {
-      messages.push(chat.data());
-    });
+  if(calEvent) {
+   $eventForm.hide();
 
-    if (messages.length !== 0) {
-      dataArea.innerHTML = "";
-    } else {
-      dataArea.innerHTML = "<p>No messages</p>";
+    $event.children('header').html(`<i class="fa fa-calendar-o"></i>`+calEvent.title);
+    $event.find('.location').text(calEvent.location ? calEvent.location : '(No location information.)');
+    $event.find('.details').text(calEvent.details ? calEvent.details : '');
+
+    $event.show();
+  }
+  else {
+    $event.hide();
+    $('#event-start').val(start.format('YYYY-MM-DD[T]HH:mm'));
+    $('#event-end').val(end.format('YYYY-MM-DD[T]HH:mm'));
+    $eventForm.show();
+  }
+
+  var leftPosition = 0;
+  var $prong = $('.prong');
+  var prongPos = 0;
+
+  if($selectedElmt.hasClass('fc-highlight')) {
+    leftPosition = $selectedElmt.offset().left - $popup.width() + ($selectedElmt.width() / 2);
+    if(leftPosition <= 0) {
+      leftPosition = 5;
+      prongPos = $popup.width() - $selectedElmt.offset().left - 30
+    }
+    else {
+      prongPos = 15;
     }
 
-    for (let i = 0; i < messages.length; i++) {
-      const createdOn = new Date(messages[i].date.seconds * 1000);
-      dataArea.innerHTML += `
-							<article>
-								<div class="p-1 teal-blue box-shadow">
-									<p>${messages[i].message}</p>
-								</div>
-								<div class="float-right">
-									<span class="green-sheen p-05 small">
-										${messages[i].nickname}
-									</span>
-									<span class="cambridge-blue p-05 small">
-										${formatDate(createdOn)}
-									</span>
-								</div>
-							</article>
-						`;
+    $popup.css('left', leftPosition);
+    $prong.css({
+      'left': '',
+      'right': prongPos,
+      'float': 'right'
+    });
+  }
+  else {
+    leftPosition = jsEvent.originalEvent.pageX - $popup.width()/2;
+    if(leftPosition <= 0) {
+      leftPosition = 5;
+    }
+    prongPos = jsEvent.originalEvent.pageX - leftPosition - ($prong.width() * 1.7);
+
+    $popup.css('left', leftPosition);
+    $prong.css({
+      'left': prongPos,
+      'float': 'none',
+      'right': ''
+    });
+  }
+
+  var topPosition = (calEvent ? jsEvent.originalEvent.pageY : $selectedElmt.offset().top) - $popup.height() - 15;
+
+  if((topPosition <= window.pageYOffset)
+    && !((topPosition + $popup.height()) > window.innerHeight)) {
+      $popup.css('top', jsEvent.originalEvent.pageY + 15);
+      $prong.css('top', -($popup.height() + 12))
+        .children('div:first-child').removeClass('bottom-prong-dk').addClass('top-prong-dk')
+        .next().removeClass('bottom-prong-lt').addClass('top-prong-lt');
+  }
+  else {
+    $popup.css('top', topPosition);
+    $prong.css({'top': 0, 'bottom': 0})
+      .children('div:first-child').removeClass('top-prong-dk').addClass('bottom-prong-dk')
+      .next().removeClass('top-prong-lt').addClass('bottom-prong-lt');
+  }
+
+  $popup.show();
+  $popup.find('input[type="text"]:first').focus();
+}
+
+$(document).ready(function() {
+  $('#calendar').fullCalendar({
+    header: {
+    left: 'title',
+      right: 'prev,next today'
+    },
+    timezone: 'local',
+    defaultView: 'month',
+    allDayDefault: false,
+    allDaySlot: false,
+    slotEventOverlap: true,
+    slotDuration: "01:00:00",
+    slotLabelInterval: "01:00:00",
+    snapDuration: "00:15:00",
+    contentHeight: 700,
+    scrollTime :  "8:00:00",
+    axisFormat: 'h:mm a',
+    timeFormat: 'h:mm A()',
+    selectable: true,
+    events: function(start, end, timezone, callback) {
+			let arr = parselocalstorage('events')  
+      callback(arr);
+    },
+    eventColor: '#dec5c9',
+    eventClick: function (calEvent, jsEvent) {
+      
+      renderPopup(jsEvent, calEvent.start, calEvent.end, calEvent);
+
+      
+    },
+    eventRender: function(event, element) {
+            element.append( `<span class='I_delete'><i class="fa fa-remove fa-2x"></i></span>` );
+            element.append( `<span class='I_edit'><i class="fa fa-edit fa-2x"></i></span>` );
+            element.find(".I_delete").click(function() {
+            $('#calendar-popup').hide();
+            if(confirm('are you sure want to delete event?')) {
+             $('#calendar').fullCalendar('removeEvents',event._id);
+            var index=events.map(function(x){ return x.id; }).indexOf(event.id);
+            events.splice(index,1);
+            localStorage.setItem('events', JSON.stringify(events));
+           
+            events=parselocalstorage('events')   
+
+       }
+            });
+        element.find(".I_edit").click(function() {
+            $('#calendar-popup').hide();
+
+          $('#eventname').val(event.title)
+          $('#location').val(event.location)
+          $('#eventdetails').val(event.details)
+          $('input#eventstart').val(event.start._i)
+           $('input#eventend').val(event.end._i)
+          $('#simplemodal').show();
+         
+          
+          //update events
+          var that=event;
+           $('#edit-form').on('submit', function(e) {
+           e.preventDefault();
+           $form = $(e.currentTarget);
+
+         $title = $form.find('input#eventname');
+         $location = $form.find('input#location');
+         $details = $form.find('textarea#eventdetails');
+             $start= $form.find('input#eventstart');
+             $end= $form.find('input#eventend');
+            //update value
+             that.title=$title.val();
+              that.location=$location.val();
+             that.details=$details.val();
+                that.start=$start.val();               
+               that.end=$end.val();
+            
+            $('#calendar').fullCalendar('updateEvent', that);
+             console.log('after update',events)
+              $('#simplemodal').hide();
+              $('#calendar-popup').hide();
+           });
+           $('#calendar').fullCalendar('updateEvent', event);
+         
+         // 
+           // 		localStorage.setItem('events', JSON.stringify(events));
+            });
+      
+      $('#close-btnid').click(function(){
+                  $('#simplemodal').hide();
+      })
+    
+      var modal=document.getElementById('simplemodal')
+
+     window.addEventListener('click',clickOutside)
+      function clickOutside(e){
+      if(e.target==modal){
+        modal.style.display='none';
+
+        }
+        }
+        }
+    ,
+    select: function(start, end, jsEvent) {
+        $('.btn-primary').css('opacity',1)
+          $('.btn-primary').click(function(){
+        renderPopup(jsEvent, start.local(), end.local());
+      }) 
+      renderPopup(jsEvent, start.local(), end.local());
+    
     }
   });
+
+  $('#event-form').on('submit', function(e) {
+    e.preventDefault();
+
+    $form = $(e.currentTarget);
+
+    $title = $form.find('input#event-title');
+    $location = $form.find('input#event-location');
+    $details = $form.find('textarea#event-details');
+ $ID = '_' + Math.random().toString(36).substr(2, 9)
+    events.push({
+      id:$ID,
+      title: $title.val(),
+      start: $form.find('input#event-start').val(),
+      end: $form.find('input#event-end').val(),
+      location: $location.val(),
+      details: $details.val()
+    });
+
+    $title.val('');
+    $location.val('');
+    $details.val('');
+
+    $form.parent().blur().hide();
+   localStorage.setItem('events', JSON.stringify(events));
+    $('#calendar').fullCalendar('refetchEvents');
+
+  });
+
+  
+
+  //Set hide action for ESC key event
+  $('#calendar-popup').on('keydown', function(e) {
+    $this = $(this);
+    console.log($this);
+    if($this.is(':visible') && e.which === 27) {
+      $this.blur();
+    }
+  })
+  //Set hide action for lost focus event
+  .on('focusout', function(e) {
+    $this = $(this);
+    if($this.is(':visible') && !$(e.relatedTarget).is('#calendar-popup, #calendar-popup *')) {
+      $this.hide();
+    }
+  });
+});
+
+/*** TESTING/DEMO ***/
+var date = new Date();
+var today = date.getDate();
+var month = date.getMonth() + 1;
+var year = date.getFullYear();
+today = today < 10 ? '0' + today.toString() : today;
+var tomorrow = today + 1 < 10 ? '0' + (today + 1).toString() : today + 1; //today not last day
+var yesterday = today - 1 < 10 ? '0' + (today - 1).toString() : today - 1; //today not first day
+      localStorage.clear()
+
+var str=	localStorage.getItem('events');
+      var obj=JSON.parse(str)||[]
+			let arr = Object.keys(obj).map((k) => obj[k])
+      console.log('what is in aarrr1',events)
+if(events.length===0){
+  events.push(
+  {id:1,title: 'event1', start: year + '-' + month + '-' + today + 'T07:00', end: year + '-' + month + '-' + today + 'T10:00', location: 'The Moon', details: 'There will be cheese'},
+  {id:2,title: 'event2', start: year + '-' + month + '-' + tomorrow + 'T03:00', end: year + '-' + month + '-' + tomorrow + 'T08:00', location: 'The Moon', details: 'There will be cheese'},
+  {id:3,title: 'event3', start: year + '-' + month + '-' + yesterday + 'T20:00', end: year + '-' + month + '-' + today + 'T05:00', location: 'The Moon', details: 'There will be cheese'}
+);
+}
+/*events.push(
+  {title: 'event1', start: year + '-' + month + '-' + today + 'T07:00', end: year + '-' + month + '-' + today + 'T10:00', location: 'The Moon', details: 'There will be cheese'},
+  {title: 'event2', start: year + '-' + month + '-' + tomorrow + 'T03:00', end: year + '-' + month + '-' + tomorrow + 'T08:00', location: 'The Moon', details: 'There will be cheese'},
+  {title: 'event3', start: year + '-' + month + '-' + yesterday + 'T20:00', end: year + '-' + month + '-' + today + 'T05:00', location: 'The Moon', details: 'There will be cheese'}
+);*/
+
+  		localStorage.setItem('events', JSON.stringify(events));
+
+/***************/
+
+
+
+
+
+//handle search
+
+    var alreadyFilled = false;
+    function initDialog() {
+        clearDialog();
+        for (var i = 0; i < events.length; i++) {
+    var mS1 = {"01":'Jan', "02":'Feb', "03":'Mar', "04":'Apr', "05":'May', "06":'June', "07":'July', "08":'Aug', "09":'Sept', "10":'Oct', "11":'Nov', "12":'Dec'};
+
+            $('.dialog').append('<div><span class="s_title">' + events[i].title +`</span><br><span class='s_des'>"` +events[i].details+
+                                  
+                                 `</span> <span class='duration'>`+events[i].start+`</span>`
+                                    
+                                + `</div>
+   <ul class="vertical-date">
+            <li class="list-daynumber">`+events[i].start.slice(8,10)+`</li>
+            <li class="list-monthname">`+mS1[events[i].start.slice(5,7)]+`</li>
+          </ul>
+
+`);
+
+        }
+    }
+    function clearDialog() {
+        $('.dialog').empty();
+    }
+    $('.autocomplete input').click(function() {
+        if (!alreadyFilled) {
+            $('.dialog').addClass('open');
+        }
+
+    });
+    $('body').on('click', '.dialog > div', function() {
+        $('.autocomplete input').val($(this).find('.s_title').text()).focus();
+        $('.autocomplete .close').addClass('visible');
+        alreadyFilled = true;
+    });
+    $('.autocomplete .close').click(function() {
+        alreadyFilled = false;
+        $('.dialog').addClass('open');
+        $('.autocomplete input').val('').focus();
+        $(this).removeClass('visible');
+    });
+
+    function match(str) {
+        str = str.toLowerCase();
+        clearDialog();
+        for (var i = 0; i < events.length; i++) {
+            if ((events[i].title).toLowerCase().startsWith(str)) {
+              
+    var mS2 = {"01":'Jan', "02":'Feb', "03":'Mar', "04":'Apr', "05":'May', "06":'June', "07":'July', "08":'Aug', "09":'Sept', "10":'Oct', "11":'Nov', "12":'Dec'};
+
+                $('.dialog').append('<div><span class="s_title">' + events[i].title+`</span><br><span class='s_des'>` +events[i].details
+                                    +
+                                 ` </span><span class='duration'>`+events[i].start+`</span>`
+                                    +
+                                    `</div>
+   <ul class="vertical-date">
+            <li class="list-daynumber">`+events[i].start.slice(8,10)+`</li>
+            <li class="list-monthname">`+mS2[events[i].start.slice(5,7)]+`</li>
+          </ul>
+`);
+        
+            }
+        }
+    }
+    $('.autocomplete input').on('input', function() {
+        $('.dialog').addClass('open');
+        alreadyFilled = false;
+        match($(this).val());
+    });
+    $('body').click(function(e) {
+        if (!$(e.target).is("input, .close")) {
+            $('.dialog').removeClass('open');
+        }
+    });
+    initDialog();
+
+
+    function parselocalstorage(name){
+      var str= localStorage.getItem(name);
+      var obj=JSON.parse(str)||[]
+      let arr = Object.keys(obj).map((k) => obj[k])||[]
+      return arr
+    }
+
