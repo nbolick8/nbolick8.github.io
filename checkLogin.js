298b21fea63476d6aa36c9d2c4a0506769945db3
Cameron Bhatnagar <cbhatnag@uncc.edu>
2021-05-08 12:32:51 -0400

    Removed checkLogin

--- checkLogin.js
+++
@@ -1,6 +0,0 @@
-//Handle Account Status
-firebase.auth().onAuthStateChanged(user => {
-    if(!user) {
-      window.location = 'login.html'; //If User is not logged in, redirect to login page
-    }
-  });
